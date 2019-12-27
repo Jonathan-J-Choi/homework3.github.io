4e30f26889477b584bfc76197879e51ee433688e
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2019-12-27 18:32:49 -0500

    multi answer prompt

--- script.js
+++ script.js
@@ -1,139 +1,3 @@
-// // Homework 3 instructions
-// # Unit 03 JavaScript Homework: Password Generator
-
-// ## Description
-
-// Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.
-
-// The user will be prompted to choose from the following password criteria:
-
-// * Length (must be between 8 and 128 characters)
-
-// * Character type:
-
-//   * Special characters ([see examples](https://www.owasp.org/index.php/Password_special_characters))
-
-//   * Numeric characters
-
-//   * Lowercase characters
-
-//   * Uppercase characters
-
-// The application should validate user input and ensure that at least one character type is selected.
-
-// Once all prompts are answered, the user will be presented with a password matching the answered prompts. Displaying the generated password in an alert is acceptable, but attempt to write the password to the page instead.
-
-// As a bonus, the user should also have the option to click a button to copy the password to their clipboard.
-
-// Your application should have a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
-
-var pass = prompt("Please choose how long long you wish your password to be.  Please choose a whole number between 8 and 128.");
-if (pass > 7 & pass < 129) {
-  alert("You got it chief!");
-  if (confirm("Would you like uppercase letters in your password?") == true) {
-    alert("I can do that for you!");
-    if (confirm("Would you like lowercase letters in your password?") == true) {
-      alert("I can do that for you!");
-      if (confirm("Would you like numbers in your password?") == true) {
-        alert("I can do that for you!");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-          console.log (pass)
-        }
-      }
-      else {
-        alert("Not a problem");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-
-      }
-    }
-    else {
-      alert("Not a problem.")
-      if (confirm("Would you like numbers in your password?") == true) {
-        alert("I can do that for you!");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-      }
-      else {
-        alert("Not a problem");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-
-      }
-
-    }
-  }
-  else {
-    alert("Not a problem");
-    if (confirm("Would you like lowercase letters in your password?") == true) {
-      alert("I can do that for you!");
-      if (confirm("Would you like numbers in your password?") == true) {
-        alert("I can do that for you!");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-      }
-      else {
-        alert("Not a problem");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-
-      }
-    }
-    else {
-      alert("Not a problem.")
-      if (confirm("Would you like numbers in your password?") == true) {
-        alert("I can do that for you!");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-      }
-      else {
-        alert("Not a problem");
-        if (confirm("Would you like symbols in your password?") == true) {
-          alert("Generating your password!");
-        }
-        else {
-          alert("Not a problem. Generating your password!");
-        }
-
-      }
-
-    }
-  }
-}
-
-
-else {
-  alert("Not a valid password length");
-}
-
 // Dom Elements
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
@@ -220,3 +84,160 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+var length = Number(prompt("How many characters will your password be? Please enter a number between 8 and 128"));
+//ask for character type
+var charType = prompt("Please enter any combination of the following character types in any order with or without all of them: lowercase, uppercase, numeric, or special.  examples include: 'lowercase, special' or 'special, uppercase, numeric, lowercase.' or 'numeric, uppercase, lowercase'");
+//generate password
+function generatePassword() {
+  //evaluate character type
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "lowercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz0123456789";
+  } else if( charTypeLower === "lowercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  } else if( charTypeLower === "uppercase, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "uppercase, numeric" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if( charTypeLower === "uppercase, special" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  } else if( charTypeLower === "numeric, lowercase" ) {
+    charSet = "0123456789abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "numeric, uppercase" ) {
+    charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric, special" ) {
+    charSet = "0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  
+  } else if( charTypeLower === "special, lowercase" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "special, uppercase" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "special, numeric" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+
+  } else if( charTypeLower === "lowercase, uppercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
+  } else if( charTypeLower === "lowercase, uppercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; 
+  } else if( charTypeLower === "lowercase, numeric, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "lowercase, numeric, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, special, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "lowercase, special, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789";
+
+  } else if( charTypeLower === "uppercase, lowercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if( charTypeLower === "uppercase, lowercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, numeric, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if( charTypeLower === "uppercase, numeric, special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  } else if( charTypeLower === "uppercase, special, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, special, numeric" ) {
+    charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  } else if( charTypeLower === "numeric, lowercase, uppercase" ) {
+    charSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric, lowercase, special" ) {
+    charSet = "0123456789abcdefghijklmnopqrstuvwxyz !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "numeric, uppercase, lowercase" ) {
+    charSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric, uppercase, special" ) {
+    charSet = "0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric, special, lowercase" ) {
+    charSet = "0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "numeric, special, uppercase" ) {
+    charSet = "0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  } else if( charTypeLower === "special, lowercase, uppercase" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "special, lowercase, numeric" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyz0123456789";
+  } else if( charTypeLower === "special, uppercase, lowercase" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "special, uppercase, numeric" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "special, numeric, lowercase" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "special, numeric, uppercase" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  } else if( charTypeLower === "lowercase, uppercase, numeric, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, uppercase, special, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, numeric, uppercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, numeric, special, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, special, uppercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "lowercase, special, numeric, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  } else if( charTypeLower === "uppercase, lowercase, numeric, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, lowercase, special, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, numeric, lowercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, numeric, special, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, special, lowercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "uppercase, special, numeric, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  } else if( charTypeLower === "numeric, lowercase, uppercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "numeric, lowercase, special, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "numeric, uppercase, lowercase, special" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "numeric, uppercase, special, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "numeric, special, lowercase, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "numeric, special, uppercase, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  } else if( charTypeLower === "special, lowercase, uppercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "special, lowercase, numeric, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "special, uppercase, lowercase, numeric" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "special, uppercase, numeric, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "special, numeric, lowercase, uppercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } else if( charTypeLower === "special, numeric, uppercase, lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  }
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+alert(generatePassword());
