ce03acb5a3f697ce7985cad0b086556286e0fc8f
Jonathan Choi <Jonathan.Junghoon.Choi@gmail.Com>
2019-12-27 16:31:18 -0500

    instructions added for visual help

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+// // Homework 3 instructions
+// # Unit 03 JavaScript Homework: Password Generator
+
+// ## Description
+
+// Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.
+
+// The user will be prompted to choose from the following password criteria:
+
+// * Length (must be between 8 and 128 characters)
+
+// * Character type:
+
+//   * Special characters ([see examples](https://www.owasp.org/index.php/Password_special_characters))
+
+//   * Numeric characters
+
+//   * Lowercase characters
+
+//   * Uppercase characters
+
+// The application should validate user input and ensure that at least one character type is selected.
+
+// Once all prompts are answered, the user will be presented with a password matching the answered prompts. Displaying the generated password in an alert is acceptable, but attempt to write the password to the page instead.
+
+// As a bonus, the user should also have the option to click a button to copy the password to their clipboard.
+
+// Your application should have a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
 
 var pass = prompt("Please choose how long long you wish your password to be.  Please choose a whole number between 8 and 128.");
 if (pass > 7 & pass < 129) {
-  alert("You got it cheif!");
+  alert("You got it chief!");
   if (confirm("Would you like uppercase letters in your password?") == true) {
     alert("I can do that for you!");
     if (confirm("Would you like lowercase letters in your password?") == true) {
@@ -13,6 +41,7 @@ if (pass > 7 & pass < 129) {
         }
         else {
           alert("Not a problem. Generating your password!");
+          console.log (pass)
         }
       }
       else {
@@ -105,10 +134,6 @@ else {
   alert("Not a valid password length");
 }
 
-
-// confirm("Would you like lowercase letters in your password?");
-// confirm("Would you like numbers in your password?");
-// confirm("Would you like symbols in your password?");
 // Dom Elements
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
@@ -194,3 +219,4 @@ function getRandomSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.'
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
